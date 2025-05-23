bb3dea279779719b3a2033ad7b9417cd68d1e95f
Rifki Al Sauqy <rifkialsauqy@students.usu.ac.id>
2025-05-23 18:05:37 +0700

    latihan1.cpp

--- Pertemuan 4/latihan1.cpp
+++ Pertemuan 4/latihan1.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //header cipupus/cpp
+using namespace std; //gak memakai std berulang
 
 int main(){
     string kalimat;
     int i;
 
-    cout<<"Masukkan kalimat: "; getline(cin,kalimat);
-
-    for(i = 0; i <= kalimat.length(); i++){
-        kalimat[i] = toupper(kalimat[i]);
+    cout<<"Masukkan kalimat: "; getline(cin,kalimat); //getline untuk bisa membaca karakter spasi
+    //for itu adalah perulangan
+    for(i = 0; i <= kalimat.length(); i++){ // kalimat.length membaca panjang string kalimat
+        kalimat[i] = toupper(kalimat[i]); //toupper untuk membuat semua kalimat jadi kapital atau huruf besar
     }
     cout<<"Kalimat dalam huruf kapital: "<<kalimat<<endl;
 }
