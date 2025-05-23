8c3f6708faca18809d70f7fce2b2f7d92bc18738
Rifki Al Sauqy <rifkialsauqy@students.usu.ac.id>
2025-05-23 19:22:43 +0700

    latihanArray.cpp

--- Pertemuan 5/latihanArray.cpp
+++ Pertemuan 5/latihanArray.cpp
@@ -1,24 +1,25 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //header cipupus/cpp
+using namespace std; //gak perlu ribet memakai std
 int main(){
-    int matrix1[2][2];
+    //membuat array untuk matrix 2x2
+    int matrix1[2][2]; 
     int matrix2[2][2];
     int matrix_hasil[2][2];
-
+    //input data matrix ke satu
     cout<<"Matrix 1:"<<endl;
     for (int i=0;i<2;i++){
         for (int j=0;j<2;j++){
             cout<<"Masukkan baris "<<i + 1<<" kolom "<< j + 1<<" : ";cin>>matrix1[i][j];
         }
     }
-
+    //input data matrix ke dua
     cout<<"Matrix 2:"<<endl;
     for (int i=0;i<2;i++){
         for (int j=0;j<2;j++){
             cout<<"Masukkan baris "<<i + 1<<" kolom "<< j + 1<<" : ";cin>>matrix2[i][j];
         }
     }
-
+    //menjumlahkan 2 matrix sekaligus menampilkan matrix 2x2
     cout<<"\nHasil:"<<endl;
     for (int i=0;i<2;i++){
         for (int j=0;j<2;j++){
