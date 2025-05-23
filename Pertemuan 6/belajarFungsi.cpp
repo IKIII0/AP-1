bf80a271e8ac46ccdc5df5b5b65b292ebfd3585f
Rifki Al Sauqy <rifkialsauqy@students.usu.ac.id>
2025-05-23 19:46:05 +0700

    belajarFungsi.cpp

--- Pertemuan 6/belajarFungsi.cpp
+++ Pertemuan 6/belajarFungsi.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
-using namespace std;
-
+#include <iostream> // header cipupus/cpp
+using namespace std; //tidak perlu memakai std
+//membuat fungsi sapa dan memakai parameter formal
 void sapa(string nama){
     cout<<"Halo "<<nama<<"! Selamat belajar CIPUPUS"<<endl;
 }
 
 int main(){
     string nama="IKI";
+    //menampilkan fungsi tersebut dan memakai parameter aktual
     sapa(nama);
-
-
 }
