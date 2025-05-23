05bf7ed86e7e48381bd6549686f305f46d496c3a
Rifki Al Sauqy <rifkialsauqy@students.usu.ac.id>
2025-05-23 17:51:13 +0700

    Latihan.cpp

--- Pertemuan 3/Latihan.cpp
+++ Pertemuan 3/Latihan.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
+#include <iostream> //header cipupus
 
-using namespace std;
+using namespace std; //biar ga perlu std berulang
 int main(){
     int nilai;
-    cout<<"Masukkan nilai: ";cin>>nilai;
+    cout<<"Masukkan nilai: ";cin>>nilai; // cin untuk input
 
-    if (nilai % 10 == 0){
-        cout<<nilai<<" kelipatan 10";
+    if (nilai % 10 == 0){ //if untuk mengecek kondisi
+        cout<<nilai<<" kelipatan 10"; // cout untuk nampilin
     }
-    else if (nilai % 5 == 0){
+    else if (nilai % 5 == 0){ //else if jika kondisi yang sebelumnya tidak terpenuhi
         cout<<nilai<<" kelipatan 5";
     }
-    else{
+    else{ //else jika semua kondisi tidak terpenuhi
         cout<<nilai<<" bukan keduanya";
     }
 }
