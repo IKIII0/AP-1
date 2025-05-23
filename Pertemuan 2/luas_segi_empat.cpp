8d6890e7be3806d8d49114a32694d83f0a07a235
Rifki Al Sauqy <rifkialsauqy@students.usu.ac.id>
2025-05-23 17:47:24 +0700

    luas_segi_empat.cpp

--- Pertemuan 2/luas_segi_empat.cpp
+++ Pertemuan 2/luas_segi_empat.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
+#include <iostream> //header cipupus/cpp/c++
 
-using namespace std;
+using namespace std; //biar gak perlu memakai std berulang
 
 int main(){
     float luas, p, l;
 
-    cout<<"Masukkan panjang: "; cin>>p;
-    cout<<"Masukkan lebar: "; cin>>l;
+    cout<<"Masukkan panjang: "; cin>>p; //cout untuk nampilin
+    cout<<"Masukkan lebar: "; cin>>l; //cin untuk input
 
     luas = p*l;
-    cout<<"Luas: "<<luas<<endl;
+    cout<<"Luas: "<<luas<<endl; //endl untuk newline
 }
