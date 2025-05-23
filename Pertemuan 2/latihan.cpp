aedd4861654b2dfae4d09691a9f9428b878b1f7b
Rifki Al Sauqy <rifkialsauqy@students.usu.ac.id>
2025-05-23 17:46:07 +0700

    latihan.cpp

--- Pertemuan 2/latihan.cpp
+++ Pertemuan 2/latihan.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //header cipupus/cpp/c++
+using namespace std; //untuk tidak perlu menggunakan std berulang lagi
 
 int main(){
     float v,r,lp;
 
-    cout<<"Masukkan Jari jari: ";cin>>r;
+    cout<<"Masukkan Jari jari: ";cin>>r; //cin untuk input data
     v = (float(4)/3) * 3.14 * r * r * r;
     lp = 4 * 3.14 * r*r;
-    cout<<"Hasil Volume: "<<v<<endl;
-    cout<<"Hasil Luas Permukaan: "<<lp<<endl;
+    cout<<"Hasil Volume: "<<v<<endl; //endl untuk newline atau enter kebawah
+    cout<<"Hasil Luas Permukaan: "<<lp<<endl; //cout untuk nampilin data
 }
