67ca31dc23a1cbb0588d95439809644de1d535d7
Rifki Al Sauqy <rifkialsauqy@students.usu.ac.id>
2025-05-24 21:15:14 +0700

    Pertemuan 6: Fungsi & Header

--- Pertemuan 6/header.cpp
+++ Pertemuan 6/header.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-#include <cmath>
-#include "headerCustom.h"
-u;
-
+#include <iostream> // header cipupus
+#include <cmath> // header cmath disini untuk sqrt
+#include "headerCustom.h" // memanggil header buatan 
+u; //using namespace std yang diubah pada headerCustom.h
+//fungsi perjumlahan
 int jumlah(int a, int b){
     return a + b;
 }
-
+//fungsi pengurangan
 int kurang(int a, int b){
     return a - b;
 }
-
+//program utama
 int main() {
-    gedi;
+    gedi; // system("cls") yang diubah pada headerCustom.h
     int n;
 
-    print L jumlah(2,3) L e;
-    print L kurang(5,3) L e;
+    print L jumlah(2,3) L e; // << menjadi L yang diubah pada headerCustom.h
+    print L kurang(5,3) L e; // endl menjadi e yang diubah pada headerCustom.h
     double Akar = 25.0;
     double akar = sqrt(Akar);
     print L "Akar dari " L Akar L " adalah " L akar L e;
