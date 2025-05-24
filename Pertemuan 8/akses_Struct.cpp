fd57e704aeaffe709731dc1198dcf3d23fa6d135
Rifki Al Sauqy <rifkialsauqy@students.usu.ac.id>
2025-05-24 21:58:34 +0700

    Pertemuan 8: Struct and Class

--- Pertemuan 8/akses_Struct.cpp
+++ Pertemuan 8/akses_Struct.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include <iostream> //header cipupus/cpp
+#include <string> //header string
+using namespace std; //biar gak pakau std
+//deklarasi struct:
 struct mahasiswa{
     string nama;
     int umur;
@@ -9,14 +10,17 @@ struct mahasiswa{
 
 int main(){
     system("cls");
-    mahasiswa mhs1;   
-    mhs1.nama = "Rifki";
+    mahasiswa mhs1; //mendeklarasikan struct di program utama
+    //menginisialisasi isi dari struct mahasiswa yang sudah disimpan di mhs1
+    mhs1.nama = "Rifki"; 
     mhs1.umur = 18;
     mhs1.ipk = 4.0;
+    //menampilkan isi struct mengguankan tanda ( . )
     // cout << "Nama: " << mhs1.nama << endl;
     // cout << "Umur: " << mhs1.umur << endl;
     // cout << "IPK: " << mhs1.ipk << endl;
 
+    //menampilkan isi struct menggunakan pointer dan tanda ( -> )
     mahasiswa *ptmhs = &mhs1;
     cout << "Nama: " << ptmhs->nama << endl;
     cout << "Umur: " << ptmhs->umur << endl;  
