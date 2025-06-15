63f9c852bdbc7d70f7b0c89e76ce85bcaedbf25a
Rifki Al Sauqy <rifkialsauqy@students.usu.ac.id>
2025-06-15 14:08:34 +0700

    PROJECT_LAB_UAS_KELOMPOK_GEDOY.cpp

--- PROJECT AKHIR/PROJECT_LAB_UAS_KELOMPOK_GEDOY.cpp
+++ PROJECT AKHIR/PROJECT_LAB_UAS_KELOMPOK_GEDOY.cpp
@@ -4,14 +4,15 @@
 #include <iomanip> //header untuk manipulasi output
 #include <cmath> //header untuk fungsi matematika
 
-using namespace std;
-using namespace chrono;
+using namespace std; //untuk tidak perlu menggunakan std berulang
+using namespace chrono; //untuk fungsi waktu 
 
-vector<string> merk_kendaraan;
-vector<string> plat_kendaraan;
-vector<string> warna_kendaraan;
-vector<system_clock::time_point> waktu_kendaraan;
+vector<string> merk_kendaraan; // vector untuk merk kendaraan 
+vector<string> plat_kendaraan; // vector untuk plat kendaraan
+vector<string> warna_kendaraan; // vector untuk warna kendaraan
+vector<system_clock::time_point> waktu_kendaraan; // vector untuk fungsi waktu ketika kendaraan pertama kali diinput
 
+// class kendaraan
 class kendaraan{
 public:
     string plat;
@@ -19,32 +20,39 @@ public:
     string warna;
 };
 
+// struct nama petugas
 struct petugas{
     string nama;
     string id;
 };
 
+// fungsi untuk menambah kendaraan
 void tambah_kendaraan(string merk, string plat, string warna){
-    merk_kendaraan.push_back(merk);
+    //push_back untuk menambah sesuatu ke vector yang diinginkan
+    merk_kendaraan.push_back(merk); 
     plat_kendaraan.push_back(plat);
     warna_kendaraan.push_back(warna);
     waktu_kendaraan.push_back(system_clock::now());
 }
 
+//fungsi untuk hiasan
 void hiasan(){
     cout << "=====================================================================================================" << endl;
 }
 
+// fungsi untuk menampilkan kendaraan
 void tampilkan_semua_kendaraan(){
     system("cls");
+    // mengecek kendaraan kosong atau tidak
     if (merk_kendaraan.empty()){
         cout << "Tidak Ada Data Kendaraan" << endl;
-        return;
+        return; //fungsi return untuk mengembalikan nilai dan menghentikan eksekusi fungsi
     }
+    //menampilkan daftar mobil yang sudah terparkir
     cout << "DAFTAR MOBIL YANG SUDAH TERPARKIR :" << endl;
     hiasan();
     for (size_t i = 0; i < merk_kendaraan.size(); i++){
-        time_t waktu_masuk = system_clock::to_time_t(waktu_kendaraan[i]);
+        time_t waktu_masuk = system_clock::to_time_t(waktu_kendaraan[i]); 
         system_clock::time_point waktu_sekarang = system_clock::now();
         duration<double> durasi = waktu_sekarang - waktu_kendaraan[i];
 
@@ -60,20 +68,21 @@ void tampilkan_semua_kendaraan(){
     }
 }
 
+//fungsi untuk pembayaran parkir
 void pembayaran(){
     balik:
     system("cls");
-    tampilkan_semua_kendaraan();
-    if (merk_kendaraan.empty()){
+    tampilkan_semua_kendaraan(); //menampilkan semua kendaraan yang sudah terparkir terlebih dahulu
+    if (merk_kendaraan.empty()){ //mengecek apakah kosong atau tidak
         cout << "Tidak Ada Kendaraan Untuk Pembayaran" << endl;
         return;
     }
     string plat;
-    cin.ignore();
+    cin.ignore(); //untuk mengampil newline
     cout << "Masukkan Plat Mobil yang akan keluar: "; getline(cin, plat);
 
     for (size_t i = 0; i < plat_kendaraan.size(); i++){
-        if (plat_kendaraan[i] == plat){
+        if (plat_kendaraan[i] == plat){ //mengecek apakah sama plat yang diinput
             system_clock::time_point waktu_keluar = system_clock::now();
             duration<double> durasi = waktu_keluar - waktu_kendaraan[i];
 
@@ -81,7 +90,7 @@ void pembayaran(){
             cout << "Mobil dengan plat " << plat << " telah parkir selama " << int(durasi.count()) << " detik." << endl;
             cout << "B I A Y A  P A R K I R : Rp" << biaya << endl;
 
-            // Hapus kendaraan dari daftar
+            // Hapus kendaraan dari daftar menggunakan fungsi erase
             merk_kendaraan.erase(merk_kendaraan.begin() + i);
             plat_kendaraan.erase(plat_kendaraan.begin() + i);
             warna_kendaraan.erase(warna_kendaraan.begin() + i);
@@ -91,10 +100,11 @@ void pembayaran(){
         }
     }
     cout << "Plat mobil tidak ditemukan!" << endl;
-    goto balik;
+    goto balik; //fungsi balik
     
 }
 
+//fungsi untuk menghias awalan
 void hiasan_utama(){
     cout << "=====================================================================================================" << endl;
     cout << "-------------------------------------- S E L A M A T D A T A N G ------------------------------------" << endl;
@@ -107,13 +117,14 @@ void hiasan_utama(){
 int main(){
     system("cls");
     petugas petugas;
-    string *nama_petugas = &petugas.nama,*id_petugas = &petugas.id;
+    string *nama_petugas = &petugas.nama,*id_petugas = &petugas.id; //membuat pointer dengan berisikan alamat nama petugas dan id dari petugas
     kendaraan mobil;
     int pilih, jumlah_mobil;
 
     hiasan_utama();
     cout << "Kami senang Anda telah hadir." << endl;
     cout << "Sebelum melanjutkan, silahkan masukkan informasi Anda yang akan bertugas hari ini.\n" << endl;
+
 
     cout << "Masukkan nama : "; getline(cin, *nama_petugas);
     cout << "Masukkan ID   : "; cin >> *id_petugas;
@@ -122,12 +133,14 @@ int main(){
         system("pause");
         system("cls");
         hiasan();
+        //menampilkan nama dan i ddari petugas
         cout << "Nama Petugas : " << *nama_petugas << endl;
         cout << "ID Petugas   : " << *id_petugas << endl;
         cout << "                                     TERIMA KASIH ATAS KERJASAMANYA !                                " << endl;
         cout << "------------------------------------- S E L A M A T  B E R T U G A S --------------------------------" << endl;
         hiasan();
         cout << "\n\n";
+        //memilih opsi yang telah disediakan
         cout << "Silahkan input opsi berikut dalam bentuk angka." << endl;
         cout << "1. Tambahkan Daftar kendaraan" << endl;
         cout << "2. Tampilkan Daftar kendaraan" << endl;
@@ -135,6 +148,7 @@ int main(){
         cout << "4. Keluar Program" << endl;
         cout << "Masukkan opsi : "; cin >> pilih;
 
+        //menambah mobil
         if (pilih == 1){
             cout << "Masukan jumlah mobil yang akan dipakirkan : "; cin >> jumlah_mobil;
             cin.ignore();
@@ -147,16 +161,20 @@ int main(){
                 cout << "\n";
             }
         }
+        //menampilkan semua kendaraan yang telah di masukkan
         else if (pilih == 2){
             tampilkan_semua_kendaraan();
         }
+        //fitur untuk pembayaran parkir
         else if (pilih == 3){
             pembayaran();
         }
+        //keluar dari program
         else if (pilih == 4){
             cout << "Terima kasih telah menggunakan layanan kami." << endl;
             break;
         }
+        //jika memilih selain opsi
         else{
             cout << "Pilihan tidak valid. Program akan keluar." << endl;
             return 0;
